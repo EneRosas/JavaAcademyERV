06e30ade553b96a1586034ad65391582e48c723d
EneRosas <57964460+EneRosas@users.noreply.github.com>
2019-11-28 17:31:48 -0600

    Ejercicios Dia 28 NOV

---
+++ Ejercicio1.java
@@ -0,0 +1,12 @@
+public class Ejercicio1 {
+	
+	public static void main(String[] args){
+		int var1 = 10;
+		float var2 = 12.5f;
+		String var3 = "Java Programming";
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	}
+}
